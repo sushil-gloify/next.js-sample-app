c88614305aa0551716ac6038f095c1dfc01de5de
Sushil Dhore <sushildhore26@gmail.com>
2023-08-08 14:50:53 +0530

    modified script/file

--- scripts/app_start.sh
+++ scripts/app_start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-cd /home/ubuntu/Pipeline-Demo-Next.js/aws-codepipeline-next.js-demo-app && /bin/sh start-app.sh
+cd /home/ubuntu/Pipeline-Demo-Next.js/aws-codepipeline-next.js-demo-app && /bin/sh deploy.sh
